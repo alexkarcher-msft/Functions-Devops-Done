1ce6e94b784db4e95afe4639d4119bcb2393f113
Alex Karcher <alkarche@microsoft.com>
2019-05-06 09:05:25 -0700

    Change HTTP response

--- MyHttpTrigger.cs
+++ MyHttpTrigger.cs
@@ -26,7 +26,7 @@ namespace Functions_DevOps_Done
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Well hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
